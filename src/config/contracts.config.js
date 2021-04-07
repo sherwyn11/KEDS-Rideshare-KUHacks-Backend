cc8903dc6638f5bb5e4585e93948e7ba6fa78ea7
Sherwyn Dsouza <sherwyndsouza1999@gmail.com>
2021-04-07 20:51:38 +0530

    :hammer: fixed small error

--- src/config/contracts.config.js
+++ src/config/contracts.config.js
@@ -1,9 +1,8 @@
 import Web3 from 'web3';
-import contract from 'truffle-contract';
 import RideManager from '../contracts/RideManager.json';
 
 let accounts;
-let lms;
+let contract;
 var web3;
 
 const initContract = async () => {
@@ -13,16 +12,12 @@ const initContract = async () => {
         web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
     }
 
-    const LMS = contract(RideManager, '0x76105917F3a4B6D95676c7A2FD80c2EF19d7d879');
-    LMS.setProvider(web3.currentProvider);
-
-    accounts = await web3.eth.getAccounts();
-    lms = await LMS.deployed();
+    contract = new web3.eth.Contract(RideManager.abi, '0x76105917F3a4B6D95676c7A2FD80c2EF19d7d879');
 }
 
-const getLMSandAccount = () => {
-    return { accounts, lms };
+const getContractandAccount = () => {
+    return { accounts, contract };
 }
 
-export { initContract, getLMSandAccount };
+export { initContract, getContractandAccount };
 
